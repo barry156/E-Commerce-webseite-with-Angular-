ed221d801770f97980e82bf5a11619707781426a
Mario <killercreepermario@gmail.com>
2024-01-08 01:25:24 +0100

    Added Functions to Remove and Add Article, Customer, Order

--- Program.cs
+++ Program.cs
@@ -11,24 +11,31 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Test Reader
+        Console.WriteLine("Test Reader");
+        // Setup
         IShopReader shopReaderObj = new ShopReader();
+        Console.WriteLine("Reader Setup Done");
 
-        List<Customer> customerList = shopReaderObj.ReadAllCustomer();
-
+        // Test ReadAllCustomer
+        Console.WriteLine("Test ReadAllCustomer");
+        List <Customer> customerList = shopReaderObj.ReadAllCustomer();
         foreach (var customer in customerList)
         {
             Console.WriteLine($"ID: {customer.ID}, Name: {customer.Name}, Email: {customer.Email}, Bestellungen: {customer.Bestellungen}");
         }
 
+        // Test ReadAllArticles
+        Console.WriteLine("Test ReadAllArticles");
         List<Article> articleList = shopReaderObj.ReadAllArticles();
-
         foreach (var article in articleList)
         {
             Console.WriteLine($"ID: {article.ID}, Name: {article.Name}, Preis: {article.Preis}");
         }
 
-        List <Order> orderList = shopReaderObj.ReadAllOrders();
-
+        // Test ReadAllOrders + GetArticlesByOrderID
+        Console.WriteLine("Test ReadAllOrders + GetArticlesByOrderID");
+        List<Order> orderList = shopReaderObj.ReadAllOrders();
         foreach (var order in orderList)
         {
             Console.WriteLine($"ID: {order.ID}, Payd: {order.Payd}, Preis: {order.TotalPrice}");
@@ -38,5 +45,61 @@ class Program
                 Console.WriteLine($"ID: {article.ID}, Name: {article.Name}, Preis: {article.Preis}");
             }
         }
+
+        // Test ReadArticleByID
+        Console.WriteLine("Test ReadArticleByID");
+        Article articleAlone = shopReaderObj.ReadArticleByID(1);
+        Console.WriteLine($"ID: {articleAlone.ID}, Name: {articleAlone.Name}, Preis: {articleAlone.Preis}");
+
+        // Test Writer
+        Console.WriteLine("Test Writer");
+        // Setup
+        IShopWriter shopWriterObj = new ShopWriter();
+        // Artikels
+        Console.WriteLine("Test Artikel");
+        int i = shopWriterObj.AddArticle(new Article
+            {
+                ID      = 999,
+                Name    = "DeleteNameTest",
+                Preis   = 999.99m
+            }
+        );
+        int j = shopWriterObj.DeleteArticle(999);
+        Console.WriteLine($"ADD: {i}, DELETE: {j}");
+
+        // Kunden
+        Console.WriteLine("Test Kunden");
+        int n = shopWriterObj.AddCustomer(new Customer 
+            {
+                ID          = 999,
+                Name        = "DeleteCustomerTest",
+                Password    = "123456789",
+                Email       = "test@mail.com"
+                // Keine Bestellung da ich nicht weis wie die implementiert wird
+            }
+        );
+        int m = shopWriterObj.DeleteCustomer(999);
+        Console.WriteLine($"ADD: {n}, DELETE: {m}");
+
+        // Bestellungen
+        Console.WriteLine("Test Bestellungen");
+        List<Article> articleListForOrderTest = new List<Article>();
+        articleListForOrderTest.Add(new Article
+            {
+                ID = 1, //Artikel muss existieren
+                Name = "DeleteNameTest",
+                Preis = 999.99m
+            }
+        );
+        int p = shopWriterObj.AddOrder(new Order 
+            {
+                ID = 999,
+                ArticleList = articleListForOrderTest,
+                Payd = false,
+                TotalPrice = 999.99m
+            }
+        );
+        int q = shopWriterObj.RemoveOrder(999);
+        Console.WriteLine($"ADD: {p}, DELETE: {q}");
     }
 }
---
+++ ShopWriter.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+class ShopWriter : IShopWriter
+{
+    private string connectionString = "Data Source=DESKTOP-ODAGOGJ\\SQLEXPRESS;Initial Catalog=ShopDB;User ID=shopUser;Password=123456789;";
+    private SqlConnection connection;
+
+    public ShopWriter()
+    {
+        connection = new SqlConnection(connectionString);
+    }
+
+    // Artikel
+    public int AddArticle(Article article) 
+    {
+        try
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO Artikel (ID, Name, Preis) VALUES (@ID, @Name, @Preis)", connection);
+            command.Parameters.AddWithValue("@ID", article.ID);
+            command.Parameters.AddWithValue("@Name", article.Name);
+            command.Parameters.AddWithValue("@Preis", article.Preis);
+
+            command.ExecuteNonQuery();
+            Console.WriteLine($"Artikel mit ID {article.ID} erfolgreich angelegt.");
+
+            connection.Close();
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine("Fehler beim Hinzufügen des Artikels: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return 0;
+        }
+        return 1;
+    }
+
+    public int DeleteArticle(int articleID)
+    {
+        int rowsAffected = -1;
+        try
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM Artikel WHERE ID = @ArticleID", connection);
+            command.Parameters.AddWithValue("@ArticleID", articleID);
+            rowsAffected = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine($"Artikel mit ID {articleID} erfolgreich gelöscht.");
+            }
+            else
+            {
+                Console.WriteLine($"Artikel mit ID {articleID} nicht gefunden oder konnte nicht gelöscht werden.");
+            }
+            return rowsAffected;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Fehler beim Löschen des Artikels: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return rowsAffected;
+        }
+    }
+
+    // Kunde
+    public int AddCustomer(Customer customer)
+    {
+        try
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO kunden (name, id, password, email) VALUES (@name, @id, @password, @email)", connection);
+            command.Parameters.AddWithValue("@id", customer.ID);
+            command.Parameters.AddWithValue("@name", customer.Name);
+            command.Parameters.AddWithValue("@password", customer.Password);
+            command.Parameters.AddWithValue("@email", customer.Email);
+
+            command.ExecuteNonQuery();
+            Console.WriteLine($"Kunde mit ID {customer.ID} erfolgreich angelegt.");
+
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Fehler beim Hinzufügen des Kunden: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return 0;
+        }
+        return 1;
+    }
+
+    public int DeleteCustomer(int customerId)
+    {
+        int rowsAffected = -1;
+        try
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM kunden WHERE id = @CustomerID", connection);
+            command.Parameters.AddWithValue("@CustomerID", customerId);
+            rowsAffected = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine($"Kunde mit ID {customerId} erfolgreich gelöscht.");
+            }
+            else
+            {
+                Console.WriteLine($"Kunde mit ID {customerId} nicht gefunden oder konnte nicht gelöscht werden.");
+            }
+            return rowsAffected;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Fehler beim Löschen des Kunden: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return rowsAffected;
+        }
+    }
+
+    // Bestellung
+    public int ConnectArticleAndOrder(int articleId, int orderId) 
+    {
+        try
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO BestellungsArtikel (BestellungsID, ArtikelID) VALUES (@BestellungsID, @ArtikelID)", connection);
+            command.Parameters.AddWithValue("@BestellungsID", orderId);
+            command.Parameters.AddWithValue("@ArtikelID", articleId);
+
+            command.ExecuteNonQuery();
+            Console.WriteLine($"Artikel mit der ID {articleId} mit der Bestellung mit ID {orderId} erfolgreich verbunden.");
+
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Fehler beim verbinden von Artikel mit der ID {articleId} und der Bestellung mit der ID {orderId}: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return 0;
+        }
+        return 1;
+    }
+
+    public int AddOrder(Order order)
+    {
+        bool connectionProblems = false;
+        try
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO Bestellungen (ID, Bezahlt, Gesamtpreis) VALUES (@ID, @Bezahlt, @Gesamtpreis)", connection);
+            command.Parameters.AddWithValue("@ID", order.ID);
+            command.Parameters.AddWithValue("@Bezahlt", order.Payd);
+            command.Parameters.AddWithValue("@Gesamtpreis", order.TotalPrice);
+
+            command.ExecuteNonQuery();
+            Console.WriteLine($"Bestellung mit ID {order.ID} erfolgreich angelegt.");
+
+            connection.Close();
+
+            List<Article> articleListForOrder = order.ArticleList;
+            foreach (var article in articleListForOrder)
+            {
+                int articleId = article.ID;
+                int orderId = order.ID;
+                int i = ConnectArticleAndOrder(articleId, orderId);
+                if (i == 0) 
+                {
+                    connectionProblems = true;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Fehler beim Hinzufügen des Kunden: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return 0;
+        }
+        if (connectionProblems) 
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public int RemoveArticleAndOrderConnection(int orderId) 
+    {
+        int rowsAffected = -1;
+        try
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM BestellungsArtikel WHERE BestellungsID = @orderID", connection);
+            command.Parameters.AddWithValue("@orderID", orderId);
+            rowsAffected = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine($"Bestellung mit ID {orderId} wurde erfolgreich von den Artikeln entkoppelt.");
+            }
+            else
+            {
+                Console.WriteLine($"Kopplungen welche die Bestellung mit ID {orderId} verwenden nicht gefunden oder konnte nicht gelöscht werden.");
+            }
+            return rowsAffected;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Fehler beim entkoppeln aller Artikel vond der Bestellung mit der ID {orderId}: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return rowsAffected;
+        }
+    }
+
+    public int RemoveOrder(int orderId)
+    {
+        int rowsAffected = -1;
+        try
+        {
+            // Noch keine Verwendung fuer i
+            int i = RemoveArticleAndOrderConnection(orderId);
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM Bestellungen WHERE ID = @orderID", connection);
+            command.Parameters.AddWithValue("@orderID", orderId);
+            rowsAffected = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine($"Bestellung mit ID {orderId} erfolgreich gelöscht.");
+            }
+            else
+            {
+                Console.WriteLine($"Bestellung mit ID {orderId} nicht gefunden oder konnte nicht gelöscht werden.");
+            }
+
+            return rowsAffected;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Fehler beim Löschen der Bestellung mit der ID {orderId}: " + e.Message);
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            return rowsAffected;
+        }
+    }
+}
